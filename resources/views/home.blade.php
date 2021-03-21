5490ec3e775b2538615745f3957c2a4e058f744b
even <even@evens-MBP.home>
2021-03-21 17:42:47 +0100

    userpics-folder and small changes on homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,23 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header bg-secondary">
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+                        <div class="text-white">Gåturer (Fjelleven)
+                            <div class="float-right">🤍 0</div>
                         </div>
-                    @endif
 
-                    {{ __('You are logged in!') }}
+                    </div>
+
+                    <img src={{ asset('userpics/Fjelleven/gåturer/vealøs2.jpeg') }} alt="vealøs">
+
                 </div>
             </div>
         </div>
     </div>
-</div>
+    </div>
 @endsection
