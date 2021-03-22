df16c9ad6ae5c73976c47811225395a0c74a666d
even <even@evens-MBP.home>
2021-03-22 07:28:58 +0100

    simplyfied and styled login-page  + removed search bar from nav bar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -31,7 +31,7 @@
                     <tt>H✾ME</tt>
                 </a>
 
-                <input class="form-control col-xs-3 ml-3 mr-3" type="text" placeholder="Hva vil du se?">
+                {{-- <input class="form-control col-xs-3 ml-3 mr-3" type="text" placeholder="Hva vil du se?"> --}}
 
 
                 <!-- Right Side Of Navbar -->
@@ -58,7 +58,7 @@
                             <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                                 <a class="dropdown-item" href="{{ route('logout') }}"
                                     onclick="event.preventDefault();
-                                                                                                                                                                                                                                                                                                                                                                                         document.getElementById('logout-form').submit();">
+                                                                                                                                                                                                                                                                                                                                                                                                 document.getElementById('logout-form').submit();">
                                     {{ __('Logout') }}
                                 </a>
 
