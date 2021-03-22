e9f8176bab3192b510a0739a00176a3172330416
even <even@evens-MBP.home>
2021-03-22 11:57:48 +0100

    userpage

---
+++ resources/views/users/userpage.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.app')
+
+@section('content')
+
+
+
+    <div class="container">
+
+
+        <div class="row">
+            @for ($i = 0; $i < 3; $i++)
+                <div class="col-sm-4">
+                    <div class="card mb-2 bg-warning">
+                        <h3 class="text-center">Collection</h3>
+                        <img src={{ asset('userpics/Desertdan/polarørken/goldensky.jpeg') }} alt="vealøs">
+                        <p>
+                            Bilder av polarørkenen fra områder omkring nordpolen
+                        </p>
+                    </div>
+                </div>
+            @endfor
+        </div>
+
+
+
+    </div>
+
+@endsection
