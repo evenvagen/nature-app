da038f62b81d32783ab75b41bce1f9e423d202c2
even <even@evens-MBP.home>
2021-03-24 08:17:00 +0100

    Livewire click function

--- app/Http/Livewire/Followprofile.php
+++ app/Http/Livewire/Followprofile.php
@@ -6,6 +6,19 @@ use Livewire\Component;
 
 class Followprofile extends Component
 {
+
+    public $status;
+
+    public function mount()
+    {
+        $this->status = 'følg';
+    }
+
+    public function follow()
+    {
+        $this->status = 'følger';
+    }
+
     public function render()
     {
         return view('livewire.followprofile');
--- resources/views/livewire/followprofile.blade.php
+++ resources/views/livewire/followprofile.blade.php
@@ -1,3 +1,3 @@
 <div>
-    <button class="btn btn-dark btn-sm mt-3 px-5 text-warning">Følg</button>
+    <button wire:click="follow" class="btn btn-dark btn-sm mt-3 px-5 text-warning">{{ $status }}</button>
 </div>
