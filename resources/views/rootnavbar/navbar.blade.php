e3264e100d8aba26ad8f3ba7d382de024bf3fade
even <even@evens-MBP.home>
2021-03-24 06:47:29 +0100

    removed DMs

--- resources/views/rootnavbar/navbar.blade.php
+++ resources/views/rootnavbar/navbar.blade.php
@@ -41,7 +41,7 @@
 
                             <a class="dropdown-item" href="{{ route('logout') }}"
                                 onclick="event.preventDefault();
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 document.getElementById('logout-form').submit();">
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         document.getElementById('logout-form').submit();">
                                 {{ __('Logout') }}
                             </a>
 
@@ -55,11 +55,6 @@
 
                     </li>
                 </ul>
-
-                <a class="navbar-brand ml-4" href="{{ url('/messages') }}">
-                    ∆
-                </a>
-
 
 
             @endguest
