b4ad605c4534af7fca29ca7e3154de4745ac99d3
even <even@evens-MBP.home>
2021-03-25 12:26:13 +0100

    wrapped all routes around auth in web.php

--- routes/web.php
+++ routes/web.php
@@ -11,13 +11,13 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [HomeController::class, 'index'])->name('home');
+Route::group(
+    ['middleware' => ['auth']],
+    function () {
 
-
-Route::get('/profile/{id}', [UserController::class, 'show'])->name('user.visitprofile');
-Route::get('/profile', [UserController::class, 'profile'])->name('user.profile');
-Route::view('/messages', 'user.messages')->name('user.messages');
-
-Route::get('/livewire', function () {
-    return view('livewire-fun');
-});
+        Route::get('/home', [HomeController::class, 'index'])->name('home');
+        Route::get('/profile/{id}', [UserController::class, 'show'])->name('user.visitprofile');
+        Route::get('/profile', [UserController::class, 'profile'])->name('user.profile');
+        Route::view('/messages', 'user.messages')->name('user.messages');
+    }
+);
